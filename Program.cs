658a4093b9d4a145458a3250e0067606ccd9f2fa
1hakusai1 <55519230+1hakusai1@users.noreply.github.com>
2020-03-25 21:05:02 +0900

    100 to 105

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _100to105
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            if (X / 100 >= CalcMin(X))
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+        static int CalcMin(int price)
+        {
+            int shimo2 = price % 100;
+            int min = 0;
+            for (int i = 5; i > 0; i--)
+            {
+                min += shimo2 / i;
+                shimo2 = shimo2 % i;
+            }
+            return min;
+        }
+    }
+}
